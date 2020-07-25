3455c17e294b758a5b93f01f1ec42a652ea91508
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-25 11:31:35 -0600

    start models folder anew.

---
+++ Boulangerie/BoulangerieSnippets.cs
@@ -0,0 +1,34 @@
+// public Bread()
+//     {
+//       Price = 5;
+//       TotalPrice = 0;
+//     }
+
+//  public Pastry()
+//     {
+//       Price = 2;
+//       TotalPrice = 0;
+//     }
+
+//  public void CalculatePrice()
+//     {
+//       foreach(Pastry pastry in _instances )
+//       {
+//         TotalPrice = pastry * Price;
+
+//         if(_instances % 3 == 0)
+//         {
+//           TotalPrice *= 0.83;
+//         }
+//       }
+
+//     }
+
+// public void AddItems()
+    // {
+    //   _instances.Add(this);
+    //   Id = _instances.Count;
+    // }
+
+//     using System.Collections.Generic;
+
