c716116b70111a721866cf2a11f256536edfefa4
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-27 19:27:53 -0600

    add passing test for Pastry CalculateOrder() method.

--- Boulangerie.Tests/ModelTests/PastryTests.cs
+++ Boulangerie.Tests/ModelTests/PastryTests.cs
@@ -20,9 +20,9 @@ namespace Boulangerie.Tests
     public void PastryCalculator_CalculatesPastryOrders_Int()
     {
       Pastry newPastry = new Pastry();
-      newPastry.AddItems();//this AddItems method is borrowed from the parent class. It is necessary to include it because it is the one with parameters and gives the CalculateOrder method an argument to multiply.
+      newPastry.AddItems(5);//this AddItems method is borrowed from the parent class. It is necessary to include it because it is the one with parameters and gives the CalculateOrder method an argument to multiply.
       newPastry.CalculateOrder(); // this CalculateOrder comes from the Pastry class, and requires no parameter. 
-      Assert.AreEqual( newPastry.TotalPrice);
+      Assert.AreEqual(8, newPastry.TotalPrice);
 
     }
 
